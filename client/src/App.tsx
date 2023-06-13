24a0558ed5c4348dfe76b9ee08d42f4fd06e5285
Milagros <miliguillot12@gmail.com>
2023-06-12 22:59:21 -0300

    commitDelLogin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes, } from 'react-router-dom';
 import Home from './views/home/home'
+import Login from "./views/login/login"
 import './App.css';
 import React from 'react';
 const App: React.FC = (): JSX.Element => {
@@ -7,8 +8,8 @@ const App: React.FC = (): JSX.Element => {
   return (
     <div>
       <Routes>
-        <Route path="/home" element={<Home />} />
-
+        <Route path="/home" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
 
