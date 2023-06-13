7575a396e0444616b378dc7763d3b1c378d727a1
VictorHugoRZ <v.zamoravictor@gmail.com>
2023-06-12 19:45:56 -0600

    Se creo la estructura de la LandingPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes, } from 'react-router-dom';
 import Home from './views/home/home'
 import './App.css';
 import React from 'react';
+import LandingPage from './views/landing/landing';
 const App: React.FC = (): JSX.Element => {
 
   return (
     <div>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
-
       </Routes>
     </div>
 
