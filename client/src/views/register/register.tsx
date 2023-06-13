0eb0704bfc96d511768fd4335781325cd88fc0a4
Bansant <lautaa26@gmail.com>
2023-06-13 13:38:42 -0300

    Register y validate finalizado

--- client/src/views/register/register.tsx
+++ client/src/views/register/register.tsx
@@ -1,72 +1,134 @@
-import React from 'react'
-import { useState } from 'react'
-import validate from './validate'
+import React, { useState } from 'react';
+import validate from './validate';
 
-interface formRegister {
-    account: string
-    name: string
-    lastname: string
-    email: string
-    password: string
+interface FormRegister {
+    nickname: string;
+    name: string;
+    lastname: string;
+    email: string;
+    password: string;
+}
+
+interface ErroresRegister {
+    email?: string;
+    password?: string;
+    nickname?: string;
+    name?: string;
+    lastname?: string;
 }
 
 const Register: React.FC = (): JSX.Element => {
-    const [register, setRegister] = useState<formRegister>({
-        account: "",
-        name: "",
-        lastname: "",
-        email: "",
-        password: "",
-    })
-    const [error, setErrors] = useState<formRegister>({
-        account: "",
-        name: "",
-        lastname: "",
-        email: "",
-        password: "",
-    })
+    const [register, setRegister] = useState<FormRegister>({
+        nickname: '',
+        name: '',
+        lastname: '',
+        email: '',
+        password: '',
+    });
 
-    const handleonChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [errors, setErrors] = useState<ErroresRegister>({
+        nickname: '',
+        name: '',
+        lastname: '',
+        email: '',
+        password: '',
+    });
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+
         setRegister({
             ...register,
-            [event.target.name]: event.target.value
-        })
-        // setErrors(
-        //     validate({
-        //       ...register,[event.target.name]: event.target.value
-        //     })
-        //   )
-    }
-    const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
+            [name]: value,
+        });
 
-    }
+        const updatedErrors = validate({
+            ...register,
+            [name]: value,
+        });
+
+        setErrors(updatedErrors);
+    };
+
+    const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
     return (
         <div>
             <div>
                 <form onSubmit={handleOnSubmit}>
                     <div>
-                        <label>My account</label>
-                        <input onChange={handleonChange} value={register.account} type="text" name='name' placeholder="Your account" />
+                        <label>My nickname</label>
+                        <input
+                            onChange={handleOnChange}
+                            value={register.nickname}
+                            type="text"
+                            name="nickname" // Corregido: usar "nickname" en lugar de "name"
+                            placeholder="Your nickname"
+                        />
+                        {errors.nickname && <span>{errors.nickname}</span>}
 
                         <label>Name</label>
-                        <input onChange={handleonChange} value={register.name} type="text" name='name' placeholder="Name" />
+                        <input
+                            onChange={handleOnChange}
+                            value={register.name}
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                        />
+                        {errors.name && <span>{errors.name}</span>}
 
                         <label>Lastname</label>
-                        <input onChange={handleonChange} value={register.lastname} type="text" name='lastname' placeholder="Lastname" />
+                        <input
+                            onChange={handleOnChange}
+                            value={register.lastname}
+                            type="text"
+                            name="lastname"
+                            placeholder="Lastname"
+                        />
+                        {errors.lastname && <span>{errors.lastname}</span>}
 
                         <label>Email</label>
-                        <input onChange={handleonChange} value={register.email} type="email" name='email' placeholder="Your email" />
+                        <input
+                            onChange={handleOnChange}
+                            value={register.email}
+                            type="email"
+                            name="email"
+                            placeholder="Your email"
+                        />
+                        {errors.email && <span>{errors.email}</span>}
 
-                        <label >Password</label>
-                        <input onChange={handleonChange} value={register.password} type="password" name="password" placeholder='Your password' />
-                        <button>Register</button>
+                        <label>Password</label>
+                        <input
+                            onChange={handleOnChange}
+                            value={register.password}
+                            type="password"
+                            name="password"
+                            placeholder="Your password"
+                        />
+                        {errors.password && <span>{errors.password}</span>}
+
+                        <button
+                            disabled={
+                                !register.email ||
+                                !register.password ||
+                                !register.name ||
+                                !register.lastname ||
+                                !register.nickname ||
+                                !!errors.email ||
+                                !!errors.password ||
+                                !!errors.lastname ||
+                                !!errors.nickname ||
+                                !!errors.name
+                            }>
+                            Register
+                        </button>
                     </div>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Register
-
+export default Register;
