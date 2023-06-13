707bebf6c6e6c2e8f994d5a07743f2e89504b6cb
Nicolas Perez <niicodperez@gmail.com>
2023-06-13 13:14:01 -0300

    falta la parte del edit en profile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './views/home/home'
-import Login from "./views/login/login"
-import './App.css';
-import React from 'react';
-import LandingPage from './views/landing/landing';
-import Nav from './views/Nav/nav'
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./views/home/home";
+import Login from "./views/login/login";
+import "./App.css";
+import React from "react";
+import LandingPage from "./views/landing/landing";
+import Nav from "./views/Nav/nav";
+import Profile from "./views/profile/profile";
 
 const App: React.FC = (): JSX.Element => {
   const location = useLocation();
@@ -13,15 +14,13 @@ const App: React.FC = (): JSX.Element => {
     <div>
       {location.pathname !== "/" ? <Nav /> : null}
       <Routes>
-
-        <Route path="/home" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<LandingPage />} />
-
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
-
   );
-}
+};
 
 export default App;
