8cc014ec4e734021f3579c353ead56c8428cf25b
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-06-13 17:20:40 -0500

    Delete back_end directory

--- back_end/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField()
-
-    def __str__(self):
-        return self.username
--- back_end/users/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .serializer import UserSerializer
-from .models import User
-
-# Create your views here.
-class UserView(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
