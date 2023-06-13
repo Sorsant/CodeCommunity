7e0493c545726569a78b77602908ac305a489eb4
mauro_genna <gennamauro18@gmail.com>
2023-06-13 20:13:40 -0300

    Prototipo de Q&A

--- client/src/views/QAPage/QAPage.tsx
+++ client/src/views/QAPage/QAPage.tsx
@@ -1,30 +1,41 @@
 import React, { useState } from 'react';
-import Question from '../../components/Question/Question';
-import Answer from '../../components/Answer/Answer';
-import Formulario from './Form';
+import Thread from '../../components/Answer/Hilo';
+import styles from './QandA.module.css'
+import Posteo from './Question/Post';
 
-interface PyRProps {
-    initialQuestion?: string;
-}
+const QandA: React.FC = () => {
+    const [threads, setThreads] = useState<{ id: string; tweets: string[] }[]>([]);
 
-const PyR: React.FC<PyRProps> = ({ initialQuestion }) => {
-    const [question, setQuestion] = useState(initialQuestion || '');
-    const [answer, setAnswer] = useState('');
-    const [showAnswer, setShowAnswer] = useState(false);
+    const addTweet = (threadId: string, tweet: string) => {
+        setThreads(prevThreads =>
+            prevThreads.map(thread =>
+                thread.id === threadId ? { ...thread, tweets: [...thread.tweets, tweet] } : thread
+            )
+        );
+    };
 
-    const handleSubmit = (submittedQuestion: string, submittedAnswer: string) => {
-        setQuestion(submittedQuestion);
-        setAnswer(submittedAnswer);
-        setShowAnswer(true);
+    const createThread = () => {
+        const newThreadId = String(threads.length + 1);
+        setThreads(prevThreads => [...prevThreads, { id: newThreadId, tweets: [] }]);
     };
 
     return (
-        <div className="pyr">
-            <Question question={question} />
-            {showAnswer && <Answer answer={answer} />}
-            <Formulario onSubmit={handleSubmit} />
+        <div className={styles.divQA}>
+            <h1>Q & A</h1>
+            <Posteo onSubmit={function (title: string, content: string): void {
+                throw new Error('Function not implemented.');
+            }} />
+
+            <button onClick={createThread}>Crear nuevo hilo</button>
+
+            {threads.map(thread => (
+                <div>
+                    <Thread key={thread.id} threadId={thread.id} tweets={thread.tweets} addTweet={addTweet} />
+                </div>
+            ))}
         </div>
     );
 };
 
-export default PyR;
+export default QandA;
+
