7d9440733615b6cce31f590ca68c9bd75e58ef3b
Nicolas Perez <niicodperez@gmail.com>
2023-06-14 13:16:08 -0300

    ruta de edit(pefil) y boton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
-
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './views/home/home'
-import Login from "./views/login/login"
-import './App.css';
-import React from 'react';
-import LandingPage from './views/landing/landing';
-import Nav from './views/Nav/nav'
-import QandA from './views/QAPage/QAPage'
-import Register from './views/Register/register';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./views/home/home";
+import Login from "./views/login/login";
+import "./App.css";
+import React from "react";
+import LandingPage from "./views/landing/landing";
+import Nav from "./views/Nav/nav";
+import QandA from "./views/QAPage/QAPage";
+import Register from "./views/Register/register";
 import Profile from "./views/profile/profile";
-import CommunityForm from './views/CommunityForm/communityForm';
-
+import CommunityForm from "./views/CommunityForm/communityForm";
+import Edit from "./views/profile/edit";
 
 const App: React.FC = (): JSX.Element => {
   const location = useLocation();
@@ -19,15 +18,14 @@ const App: React.FC = (): JSX.Element => {
     <div>
       {location.pathname !== "/" ? <Nav /> : null}
       <Routes>
-
         <Route path="/nosedonde" element={<CommunityForm />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Register />} />
-        <Route path='/Q&A' element={<QandA />}></Route>
+        <Route path="/Q&A" element={<QandA />}></Route>
         <Route path="/profile" element={<Profile />} />
-
+        <Route path="/edit" element={<Edit />} />
       </Routes>
     </div>
   );
