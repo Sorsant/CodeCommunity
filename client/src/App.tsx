2d4d9b209d910b063eb136e258bcb401d8ef3346
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-06-14 14:13:44 -0600

    Se crearon los componentes de las comunidades

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './views/home/home'
-import Login from "./views/login/login"
-import './App.css';
-import React from 'react';
-import LandingPage from './views/landing/landing';
-import Nav from './views/Nav/nav'
-import QandA from './views/QAPage/QAPage'
-import Register from './views/register/register';
-=======
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./views/home/home";
 import Login from "./views/login/login";
@@ -19,7 +7,6 @@ import LandingPage from "./views/landing/landing";
 import Nav from "./views/Nav/nav";
 import QandA from "./views/QAPage/QAPage";
 import Register from "./views/Register/register";
->>>>>>> bcf1c8914c9592d453a7cad1d40b6e21d1bb7eb7
 import Profile from "./views/profile/profile";
 import CommunityForm from "./views/CommunityForm/communityForm";
 import Edit from "./views/profile/edit";
