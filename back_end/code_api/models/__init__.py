81842a444ac7e79f749f02fe26bb86a2ae6b1e73
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-06-14 13:32:52 -0500

    Delete back_end directory

--- back_end/code_api/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .answer import Answer
-from .question import Question
--- back_end/code_api/models/question.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-#Create your models here.
-class Question(models.Model):
-  title = models.CharField(max_length=60)
-  description = models.TextField()
-  
-  def __str__(self):
-    return self.title
