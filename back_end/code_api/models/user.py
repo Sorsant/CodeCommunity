b871089769ef368b93b3bcbe54caf9b46427763c
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-06-14 13:43:52 -0500

    Delete back_end directory

--- back_end/code_api/models/user.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-#Create your models here.
-class User(models.Model):
-  username = models.CharField()
-  password = models.CharField()
-  
-  def __str__(self):
-    return self.username
