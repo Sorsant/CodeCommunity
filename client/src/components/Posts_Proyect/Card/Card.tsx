2b396fd614fabb007417ff6f6593e7e645f4f0d5
Nicolas Perez <niicodperez@gmail.com>
2023-06-15 10:59:49 -0300

    anda el link del posteo al detail, puse bien la ruta y termińé el componente detail

--- client/src/components/Posts_Proyect/Card/Card.tsx
+++ client/src/components/Posts_Proyect/Card/Card.tsx
@@ -3,45 +3,53 @@ import { Link } from "react-router-dom";
 import styles from "./card.module.css";
 
 interface posteos {
-    id: number;
-    user_imagen: string;
-    nickname: string;
-    description: string;
-    titulo: string;
-    imagen: string;
-    createDate: string;
-    gitHub: string;
-    likes: number;
-
+  id: number;
+  user_imagen: string;
+  nickname: string;
+  description: string;
+  titulo: string;
+  imagen: string;
+  createDate: string;
+  gitHub: string;
+  likes: number;
 }
 
 //interface User {}
 
-const postCard: React.FC<posteos> = ({ nickname, description, user_imagen, titulo, id, imagen, likes, gitHub, createDate }): JSX.Element => {
+const postCard: React.FC<posteos> = ({
+  nickname,
+  description,
+  user_imagen,
+  titulo,
+  id,
+  imagen,
+  likes,
+  gitHub,
+  createDate,
+}): JSX.Element => {
+  return (
+    <div>
+      {/*Revisar el parametro para identificar el grupo*/}
 
-    return (
+      <div>
+        <h3>{nickname}</h3>
+        <img src={user_imagen} alt={nickname} className={styles.img1} />
+        <Link to={`/detail/${id}`}>
+          <h2>{titulo}</h2>
+        </Link>
+        <h4>{description}</h4>
+        <img src={imagen} alt={imagen} className={styles.img2} />
+        <h5>{gitHub}</h5>
         <div>
-
-            {/*Revisar el parametro para identificar el grupo*/}
-
-            <div>
-                <h3>{nickname}</h3>
-                <img src={user_imagen} alt={nickname} className={styles.img1} />
-                <Link to={`/${id}`}><h2>{titulo}</h2></Link>
-                <h4>{description}</h4>
-                <img src={imagen} alt={imagen} className={styles.img2} />
-                <h5>{gitHub}</h5>
-                <div>
-                    <h5>{createDate}</h5>
-                    <h5>{likes}</h5>
-                </div>
-                <div>
-                    <p>{description}</p>
-                </div>
-            </div>
+          <h5>{createDate}</h5>
+          <h5>{likes}</h5>
         </div>
-
-    )
-}
+        <div>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default postCard;
