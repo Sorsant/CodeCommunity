dd271ea776a6b7bc12e6632e1a6f329d385af78b
Bansant <lautaa26@gmail.com>
2023-06-19 11:22:02 -0300

    action redux home/post

--- client/src/components/Redux/action.ts
+++ client/src/components/Redux/action.ts
@@ -1,13 +1,35 @@
 // actions.ts
 import { Dispatch } from 'redux';
 import { GET_ALL_POST, GetPostsAction, Post } from './types';
-import data from '../../assets/infoHomePost';
+// import data from '../../assets/infoHomePost';
+import axios from 'axios';
 
-export const getPosts = () => (dispatch: Dispatch<GetPostsAction>) => {
-    const hardcodedPosts: Post[] = data;
+// export const getPosts = () => (dispatch: Dispatch<GetPostsAction>) => {
+//     const hardcodedPosts: Post[] = data;
 
-    dispatch({
-        type: GET_ALL_POST,
-        payload: hardcodedPosts,
-    });
-};
+//     dispatch({
+//         type: GET_ALL_POST,
+//         payload: hardcodedPosts,
+//     });
+// };
+
+export const getHomePosts = () => {
+    try {
+        const endpoint = 'http://127.0.0.1:8000/codec/api/post/'
+
+        return async (dispatch: Dispatch<GetPostsAction>) => {
+            const { data } = await axios.get(endpoint);
+
+            return dispatch({
+                type: GET_ALL_POST,
+                payload: data,
+            });
+
+
+        };
+    } catch (error) {
+        throw new Error();
+    }
+}
+
+
