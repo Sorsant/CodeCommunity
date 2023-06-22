9a6de5c315c02c1887aff034bb9f2c89a3a04ce6
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-06-21 20:33:18 -0500

    Fixed superuser

--- api/code_api/models/user.py
+++ api/code_api/models/user.py
@@ -5,24 +5,25 @@ from .language import Language
 from .community import Community
 
 class AppUserManager(UserManager):
-  def create_user(self, email, password=None):
+  def create_user(self, email, username, password=None):
     if not email:
       raise ValueError('An email is required.')
     if not password:
       raise ValueError('A password is required.')
     email = self.normalize_email(email)
-    user = self.model(email=email)
+    user = self.model(email=email, username=username)
     user.set_password(password)
     user.save()
     return user
   
-  def create_superuser(self, email, password=None):
+  def create_superuser(self, email, username, password=None):
     if not email:
       raise ValueError('An email is required.')
     if not password:
       raise ValueError('A password is required.')
-    user = self.create_user(email, password)
+    user = self.create_user(email=email, username=username, password=password)
     user.is_superuser = True
+    user.is_staff = True
     user.save()
     return user
   
@@ -30,11 +31,16 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
   user_id = models.AutoField(primary_key=True)
   email = models.EmailField(max_length=80, unique=True)
   username = models.CharField(max_length=60)
+  is_staff = models.BooleanField(default=False)
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = ['username']
   objects = AppUserManager()
-  def __str__(self):
-    return self.username
+  created = models.DateTimeField(default=timezone.now, editable=False)
+  modified = models.DateTimeField(auto_now=True, editable=False)
+    
+  def save(self, *args, **kwargs):
+    self.modified = timezone.now()
+    super().save(*args, **kwargs)
 
 class UserExtras(models.Model):
     user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
@@ -44,12 +50,6 @@ class UserExtras(models.Model):
     language = models.ManyToManyField(Language, blank=True)
     community = models.ManyToManyField(Community, blank=True)
     premium = models.BooleanField(default=False)
-    created = models.DateTimeField(default=timezone.now, editable=False)
-    modified = models.DateTimeField(auto_now=True, editable=False)
-    
-    def save(self, *args, **kwargs):
-      self.modified = timezone.now()
-      super().save(*args, **kwargs)
 
     def __str__(self):
         return self.user.username
