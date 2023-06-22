a2324292703c5b6b8d52fde9c70eacf1a619edca
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-06-22 18:36:11 -0500

    fix classes

--- api/config/settings.py
+++ api/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
