2d0a7ace359d37ab554eb03e760fed2a5a16aa8f
Nicolas Perez <niicodperez@gmail.com>
2023-06-23 17:23:23 -0300

    prototipo de fakeHome y payment sin funcionar

---
+++ client/src/components/Redux/Actions/Fake/fake.js
@@ -0,0 +1,18 @@
+import { FAKE_BUTTON, FAKE_LOGGIN } from "../../action-types";
+
+export const fakeButton = () => {
+  return async (dispatch) => {
+    return dispatch({
+      type: FAKE_BUTTON,
+      payload: false,
+    });
+  };
+};
+export const fakeLoggin = () => {
+  return async (dispatch) => {
+    return dispatch({
+      type: FAKE_LOGGIN,
+      payload: true,
+    });
+  };
+};
