50d9d80953363d314fa516ee7ec012055df62d25
CarolinaQuintoman <quintoman.carolina@gmail.com>
2023-06-24 17:26:26 -0300

    fix category

--- api/code_api/views/category_view.py
+++ api/code_api/views/category_view.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 from ..serializer.category_serializer import CategorySerializer
-from ..models.comments import Comments
+from ..models.category import Category
 
 # Create your views here.
   
 class CategoryView(viewsets.ModelViewSet):
   serializer_class = CategorySerializer
-  queryset = Comments.objects.all()
+  queryset = Category.objects.all()
