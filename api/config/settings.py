1c8ea3e641f9e5630ff17b4e2f4ef3b70806e161
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-06-24 20:53:28 -0500

    Category and Users id fixed

--- api/config/settings.py
+++ api/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
