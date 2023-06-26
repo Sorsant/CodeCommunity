5b365feaf7ebdd17eaebe2fca4aa19d8733b3c6b
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-06-25 23:29:54 -0500

    Image url

--- api/code_api/models/post.py
+++ api/code_api/models/post.py
@@ -8,8 +8,8 @@ from users.models import UserAccount
 class Post(models.Model):
   user = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name='user_post')
   title = models.TextField(max_length=255)
+  image = models.CharField(max_length=255, blank=True)
   description = models.TextField(max_length=3000)
-  image = CloudinaryField('image', folder="ImagePost")
   likes = models.ManyToManyField(UserAccount, blank=True, related_name='liked_post')
   created = models.DateTimeField(default=timezone.now, editable=False)
 
@@ -21,3 +21,6 @@ class Post(models.Model):
         if isinstance(self.image, dict):
             self.image = cargar_imagen(self.image)
         super(Post, self).save(*args, **kwargs)
+        
+        
+# image = CloudinaryField('image', folder="ImagePost")
