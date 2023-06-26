c9f8dccf2b6ee485d151f08b7e04ffd0533a1bd6
Bansant <lautaa26@gmail.com>
2023-06-25 23:23:43 -0300

    asd

--- client/src/components/Redux/reducer.js
+++ client/src/components/Redux/reducer.js
@@ -1,7 +1,25 @@
 // reducers.ts
 import {
   GET_ALL_POST, FILTER_AZ, FILTER_ZA, ADD_POST, FILTER_PUBLICATIONS, GET_ALL_LANGUAGES, POST_USER, SEARCH, GET_COMMUNITY, GET_ALL_COMMUNITIES, GET_CATEGORY, GET_NEWS, USERS, GETPOSTID, FAKE_BUTTON,GET_USER_EXTRA,
-  FAKE_LOGGIN,
+  FAKE_LOGGIN,LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  USER_LOADED_SUCCESS,
+  USER_LOADED_FAIL,
+  AUTHENTICATED_SUCCESS,
+  AUTHENTICATED_FAIL,
+  PASSWORD_RESET_SUCCESS,
+  PASSWORD_RESET_FAIL,
+  PASSWORD_RESET_CONFIRM_SUCCESS,
+  PASSWORD_RESET_CONFIRM_FAIL,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAIL,
+  ACTIVATION_SUCCESS,
+  ACTIVATION_FAIL,
+  GOOGLE_AUTH_SUCCESS,
+  GOOGLE_AUTH_FAIL,
+  FACEBOOK_AUTH_SUCCESS,
+  FACEBOOK_AUTH_FAIL,
+  LOGOUT
 } from './action-types';
 
 const initialState = {
@@ -13,7 +31,11 @@ const initialState = {
   news: [],
   users: [],
   loggin: true,
-  userExtra:[]
+  userExtra:[],
+  access: localStorage.getItem('access'),
+  refresh: localStorage.getItem('refresh'),
+  isAuthenticated: null,
+  user: null
 };
 
 const reducer = (state = initialState, { type, payload }) => {
@@ -129,6 +151,65 @@ const reducer = (state = initialState, { type, payload }) => {
         userExtra: payload,
       };
     }
+    case AUTHENTICATED_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: true
+            }
+        case LOGIN_SUCCESS:
+        case GOOGLE_AUTH_SUCCESS:
+        case FACEBOOK_AUTH_SUCCESS:
+            localStorage.setItem('access', payload.access);
+            localStorage.setItem('refresh', payload.refresh);
+            return {
+                ...state,
+                isAuthenticated: true,
+                access: payload.access,
+                refresh: payload.refresh
+            }
+        case SIGNUP_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: false
+            }
+        case USER_LOADED_SUCCESS:
+            return {
+                ...state,
+                user: payload
+            }
+        case AUTHENTICATED_FAIL:
+            return {
+                ...state,
+                isAuthenticated: false
+            }
+        case USER_LOADED_FAIL:
+            return {
+                ...state,
+                user: null
+            }
+        case GOOGLE_AUTH_FAIL:
+        case FACEBOOK_AUTH_FAIL:
+        case LOGIN_FAIL:
+        case SIGNUP_FAIL:
+        case LOGOUT:
+            localStorage.removeItem('access');
+            localStorage.removeItem('refresh');
+            return {
+                ...state,
+                access: null,
+                refresh: null,
+                isAuthenticated: false,
+                user: null
+            }
+        case PASSWORD_RESET_SUCCESS:
+        case PASSWORD_RESET_FAIL:
+        case PASSWORD_RESET_CONFIRM_SUCCESS:
+        case PASSWORD_RESET_CONFIRM_FAIL:
+        case ACTIVATION_SUCCESS:
+        case ACTIVATION_FAIL:
+            return {
+                ...state
+            }
     default:
       return state;
   }
