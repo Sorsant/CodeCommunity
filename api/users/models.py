441b63a66af8ce07ebacd81f0681af6f109c0656
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-06-26 22:24:25 -0500

    re-auth users with Djoser

--- api/users/models.py
+++ api/users/models.py
@@ -1,52 +1,51 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserAccountManager(BaseUserManager):
-  def create_user(self, first_name, last_name, email, password=None):
-    if not email:
-      raise ValueError('Users must have an email address')
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
 
-    email = self.normalize_email(email)
-    email = email.lower()
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
 
-    user = self.model(
-      first_name=first_name,
-      last_name=last_name,
-      email=email,
-    )
+        user.set_password(password)
+        user.save()
 
-    user.set_password(password)
-    user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, first_name, last_name, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
 
-    return user
-  
-  def create_superuser(self, first_name, last_name, email, password=None):
-    user = self.create_user(
-      first_name,
-      last_name,
-      email,
-      password=password,
-    )
 
-    user.is_staff = True
-    user.is_superuser = True
-    user.save(using=self._db)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-    return user
-
+        return user
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
-  first_name = models.CharField(max_length=255)
-  last_name = models.CharField(max_length=255)
-  email = models.EmailField(unique=True, max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_staff = models.BooleanField(default=False)
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-  objects = UserAccountManager()
+    objects = UserAccountManager()
 
-  USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
-  def __str__(self):
-    return self.email
+    def get_full_name(self):
+        return self.first_name
+
+    def get_short_name(self):
+        return self.first_name
+    
+    def __str__(self):
+        return self.email
--- api/users/views.py
+++ api/users/views.py
@@ -1,10 +1,28 @@
-from rest_framework import viewsets, filters
+import requests
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions, status
+from rest_framework import permissions, status, viewsets, filters
 from .serializers import UserCreateSerializer, UserSerializer
+from rest_framework.authentication import TokenAuthentication
+
+from django.http import HttpResponseBadRequest
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
+
 from .models import UserAccount
 
+User = get_user_model()
+
+class UserView(viewsets.ModelViewSet):
+  serializer_class = UserSerializer
+  queryset = UserAccount.objects.all()
+  
+  filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+  search_fields = ['first_name', 'last_name']
+  ordering_fields = ['first_name', 'last_name']
 
 class RegisterView(APIView):
   def post(self, request):
@@ -23,17 +41,55 @@ class RegisterView(APIView):
 
 class RetrieveUserView(APIView):
   permission_classes = [permissions.IsAuthenticated]
+  authentication_classes = [TokenAuthentication]
 
   def get(self, request):
     user = request.user
     user = UserSerializer(user)
 
     return Response(user.data, status=status.HTTP_200_OK)
+
+
+def activate_account(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+
+        if default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+
+            return render(request, 'activation_user_success.html')
+        else:
+            return HttpResponseBadRequest('El token de activación no es válido.')
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        return HttpResponseBadRequest('El enlace de activación no es válido.')
+
   
-class UserView(viewsets.ModelViewSet):
-  serializer_class = UserSerializer
-  queryset = UserAccount.objects.all()
-  
-  filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-  search_fields = ['first_name', 'last_name']
-  ordering_fields = ['first_name', 'last_name']
+def reset_password_confirm(request, uidb64, token):
+    if request.method == 'POST':
+        new_password = request.POST.get('new_password')
+        re_new_password = request.POST.get('re_new_password')
+
+        if new_password != re_new_password:
+            messages.error(request, 'Las contraseñas no coinciden.')
+            return redirect('reset_password_confirm', uidb64=uidb64, token=token)
+
+        data = {
+            'new_password': new_password,
+            're_new_password': re_new_password,
+            'uid': uidb64,
+            'token': token
+        }
+
+        print(data)
+
+        response = requests.post('http://localhost:8000/auth/users/reset_password_confirm/', json=data)
+
+        if response.status_code == 204:
+            messages.success(request, '¡Contraseña restablecida con éxito!')
+            return redirect('http://localhost:5000/login')
+        else:
+            messages.error(request, 'Error al restablecer la contraseña.')
+
+    return render(request, 'reset_password_confirm.html', {'uid': uidb64, 'token': token})
