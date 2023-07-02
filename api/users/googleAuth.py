03897b842d90f0a0a1c02f4999c034776d507978
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-07-02 11:24:10 -0500

    Social auth fix v3.0

--- api/users/googleAuth.py
+++ api/users/googleAuth.py
@@ -6,52 +6,49 @@ import urllib.parse
 import json
 import requests
 
-class LoginGoogle(View):
-    API_URL = config('URL_BACK')
+API_URL = config('URL_BACK')
+URL_FRONT = config('URL_FRONT')
 
-    def get(self, request):
-        try:
-            response = requests.get(f"{self.API_URL}/auth/o/google-oauth2/", params={
-                "redirect_uri": f"{self.API_URL}/accounts/profile/",
-            }, cookies=request.COOKIES)
 
-            print(request.COOKIES)
-            
-            authorization_url = response.json().get("authorization_url")
-            if authorization_url:
-                return redirect(authorization_url)
-            else:
-                print("No se encontró la URL de autorización en la respuesta")
-        except requests.RequestException as error:
-            print(f"Error al realizar la solicitud: {error}")
-        return redirect("/")
-  
+def loginGoogle(self, request):
+    try:
+        response = requests.get(f"{self.API_URL}/auth/o/google-oauth2/", params={
+            "redirect_uri": f"{self.API_URL}/accounts/profile/",
+        }, cookies=request.COOKIES)
 
-class RedirectSocial(View):
-    API_URL = config('URL_BACK')
-    URL_FRONT = config('URL_FRONT')
-    
-    def get(self, request, *args, **kwargs):
-        code, state = str(request.GET['code']), str(request.GET['state'])
-        json_obj = {'code': code, 'state': state}
-        request.session["state"] = state
+        print(request.COOKIES)
         
-        url = f"{self.API_URL}/auth/o/google-oauth2/"
-        data = urllib.parse.urlencode(json_obj).encode('utf-8')
-        headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': '; '.join([f'{key}={value}' for key, value in request.COOKIES.items()]),
-        }
-        
-        response = requests.post(url, data=data, headers=headers)
-
-        if response.status_code == 201:
-            json_res = json.loads(response.text)
-            access_token = json_res['access']
-            refresh_token = json_res['refresh']
-            
-            redirect_url = f'{self.URL_FRONT}/google?access={access_token}&refresh={refresh_token}'
-            
-            return redirect(redirect_url)
+        authorization_url = response.json().get("authorization_url")
+        if authorization_url:
+            return redirect(authorization_url)
         else:
-            return HttpResponse('Error en la petición')
+            print("No se encontró la URL de autorización en la respuesta")
+    except requests.RequestException as error:
+        print(f"Error al realizar la solicitud: {error}")
+    return redirect("/")
+    
+
+def redirectGoogle(self, request, *args, **kwargs):
+    code, state = str(request.GET['code']), str(request.GET['state'])
+    json_obj = {'code': code, 'state': state}
+    request.session["state"] = state
+    
+    url = f"{self.API_URL}/auth/o/google-oauth2/"
+    data = urllib.parse.urlencode(json_obj).encode('utf-8')
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cookie': '; '.join([f'{key}={value}' for key, value in request.COOKIES.items()]),
+    }
+    
+    response = requests.post(url, data=data, headers=headers)
+
+    if response.status_code == 201:
+        json_res = json.loads(response.text)
+        access_token = json_res['access']
+        refresh_token = json_res['refresh']
+        
+        redirect_url = f'{self.URL_FRONT}/google?access={access_token}&refresh={refresh_token}'
+        
+        return redirect(redirect_url)
+    else:
+        return HttpResponse('Error en la petición')
