86cc52ab0300414938069652055f07e94e0c5085
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-07-02 16:52:08 -0500

    Test front-back auth 3rd

--- frontend/client/src/config/index.js
+++ frontend/client/src/config/index.js
@@ -1,6 +1,7 @@
 export const API_URL = 'https://codecommunity-production.up.railway.app';
-export const FRONT_URL = 'http://localhost:3000'
+export const FRONT_URL = 'https://loved-vein-production.up.railway.app'
 
 // REACT_APP_API_URL=http://localhost:8000
 // REACT_APP_API_URL=https://codecommunity-production.up.railway.app
 // REACT_APP_FRONT_URL=http://localhost:3000
+// REACT_APP_FRONT_URL=loved-vein-production.up.railway.app
--- frontend/client/src/setupProxy.js
+++ frontend/client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 	app.use(
 		'/api',
 		createProxyMiddleware({
-			target: 'http://localhost:5000',
+			target: 'https://loved-vein-production.up.railway.app',
 			changeOrigin: true,
 		})
 	);
