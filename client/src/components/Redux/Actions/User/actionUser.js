e7dfb802f35789549853b4b80dbd5f93c0e6512f
Bansant <lautaa26@gmail.com>
2023-07-03 22:50:00 -0300

    rutas

--- client/src/components/Redux/Actions/User/actionUser.js
+++ client/src/components/Redux/Actions/User/actionUser.js
@@ -3,35 +3,35 @@ import {
     getUser,
     getUserExtra
 } from '../../Reducer/HomeReducer';
-
+import { API_URL } from "../../../../config";
 export const getUsers = () => async (dispatch) => {
-    const endpoint = '/codec/api/users/';
+    const endpoint = `${API_URL}/codec/api/users/`;
     const { data } = await axios.get(endpoint);
     dispatch(getUser(data));
 };
 
 export const getUserId = (id) => async (dispatch) => {
-    const endpoint = `/codec/api/users/${id}`;
+    const endpoint = `${API_URL}/codec/api/users/${id}`;
     const { data } = await axios.get(endpoint);
     console.log(data);
     dispatch(getUser(data));
 };
 
 export const getUserExtras = () => async (dispatch) => {
-    const endpoint = '/codec/api/user_extras/';
+    const endpoint = `${API_URL}/codec/api/user_extras`;
     const { data } = await axios.get(endpoint);
     dispatch(getUserExtra(data));
 };
 export const putUserExtras = (id,InfoUserExtra) => async (dispatch) => {
     console.log(InfoUserExtra);
-    const endpoint = `/codec/api/user_extras/${id}/`;
+    const endpoint = `${API_URL}/codec/api/user_extras/${id}/`;
     const { data } = await axios.patch(endpoint,InfoUserExtra);
    
 };
 
 export const editUser = (id,putUser) => async (dispatch) => {
     console.log(putUser);
-    const endpoint = `/codec/api/users/${id}/`;
+    const endpoint = `${API_URL}/codec/api/users/${id}/`;
     const { data } = await axios.patch(endpoint,putUser);
    
 };
