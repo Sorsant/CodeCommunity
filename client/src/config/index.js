a00272bc04edafedd536bef73bf0148dde5a1561
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-07-03 19:15:23 -0500

    Deploy ready

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,5 @@
 export const API_URL = process.env.REACT_APP_API_URL;
 export const FRONT_URL = process.env.REACT_APP_FRONT_URL;
 
-// export const API_URL = https://codecommunity-production.up.railway.app
+// export const API_URL = https://codecommunity-31wr.onrender.com
 // export const API_URL = http://localhost:8000
