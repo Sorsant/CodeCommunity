4c288a285b1951c1a418dede2daed6780bbcd4dd
Nicolas Perez <niicodperez@gmail.com>
2023-07-04 12:46:04 -0300

    algo

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8000';
+export const API_URL = "https://codecommunity-31wr.onrender.com";
 export const FRONT_URL = process.env.REACT_APP_FRONT_URL;
 
 // export const API_URL = https://codecommunity-31wr.onrender.com
