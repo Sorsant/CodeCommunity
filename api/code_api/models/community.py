4467fbd5aebc211685eab94a4fa600260a3380fe
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-07-04 21:58:00 -0500

    is_delete news and community

--- api/code_api/models/community.py
+++ api/code_api/models/community.py
@@ -9,6 +9,7 @@ class Community(models.Model):
     image = models.CharField(max_length=255)
     language = models.ManyToManyField(Language)
     created = models.DateTimeField(default=timezone.now, editable=False)
+    is_delete = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
