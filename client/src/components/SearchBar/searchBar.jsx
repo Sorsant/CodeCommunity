df9c172bd270ed4a9a5c1619594241237e427fa0
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-07-08 00:31:12 -0500

    remake navbar & sidebar

--- client/src/components/SearchBar/searchBar.jsx
+++ client/src/components/SearchBar/searchBar.jsx
@@ -19,10 +19,10 @@ const SearchBar = () => {
     }
 
     return (
-        <div className={styles.divContainer}>
-            <form className={` mt-3 ${styles.search}`} role="search" onChange={handleChange}>
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                <button className={` btn btn-success ${styles.button}`} type="submit" onClick={handleSubmit}>Search</button>
+        <div>
+            <form className={` mt-2 ${styles.searchbar}`} role="search" onChange={handleChange}>
+                <input className="form-control form-control-lg me-2 col-12" type="search" aria-label="Search"></input>
+                <button className={` btn btn-lg btn-success ${styles.button}`} type="submit" onClick={handleSubmit}>Search</button>
             </form>
         </div >
     )
